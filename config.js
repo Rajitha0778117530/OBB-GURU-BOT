7d7699f304b8176bd556e56a6dd6b0f5b5e77279
Rajitha0778117530 <132959130+Rajitha0778117530@users.noreply.github.com>
2023-07-14 02:50:44 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['447710173736', 'Owner', true],
-  ['447710173736', 'Ash', true], 
+  ['940767530789', 'Owner', true],
+  ['940767530789', 'ROYAL ', true], 
   [''] 
 ] //Number of owners
 
